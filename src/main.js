f080c6d4ed1ea5006380defd38c476c3ec85d2a5
zzzzzzpc <45305834+zzzzzzpc@users.noreply.github.com>
2020-04-13 17:19:26 +0800

    correct some errors

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+require('./mock')
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   store,
